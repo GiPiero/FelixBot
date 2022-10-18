de167656ceb4fc4a0f8bff090a18069601d9b803
GiPiero <pierocorsbie@gmail.com>
2022-10-17 22:47:23 -0400

    Use ShardManagerBuilder for multiple use in guilds

--- src/main/java/com/gipiero/felixbot/MusicBot.java
+++ src/main/java/com/gipiero/felixbot/MusicBot.java
@@ -1,24 +1,34 @@
 package com.gipiero.felixbot;
 
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
 
+import javax.security.auth.login.LoginException;
 import java.util.EnumSet;
 
 public class MusicBot {
-    public static void main(String[] args) throws Exception{
-        System.out.println("Starting MusicBot");
-        String token = System.getenv("BOT_TOKEN");
-
+    private final ShardManager shardManager;
+    public MusicBot() throws LoginException {
         EnumSet<GatewayIntent> intents = EnumSet.of(
                 GatewayIntent.GUILD_MESSAGES,
                 GatewayIntent.MESSAGE_CONTENT,
                 GatewayIntent.GUILD_VOICE_STATES
         );
+        String token = System.getenv("BOT_TOKEN");
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token, intents);
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.addEventListeners(new Listener());
+        shardManager = builder.build();
+    }
 
-        JDA api = JDABuilder.createDefault(token, intents)
-                    .addEventListeners(new Listener())
-                    .build();
+    public static void main(String[] args) throws Exception{
+        System.out.println("Starting MusicBot");
+        try {
+            MusicBot bot = new MusicBot();
+        } catch (LoginException e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
     }
 }
